105d79ab78373f803191ca6cb90ae0cc5f8b89cd
Simon B. Adrian <simon.adrian@ieee.org>
2021-08-19 17:42:36 +0200

    Made BEAST compatible, still very slow
    try h=0.06 (roughy 4000 unknowns)

--- example/beast.jl
+++ example/beast.jl
@@ -5,7 +5,7 @@ using LinearAlgebra
 using FastBEAST
 
 CM = CompScienceMeshes
-
+##
 function test_beast_aca(h)
     Γ = CM.meshcuboid(1,1,1,h) # CM.read_gmsh_mesh(fn)
 
@@ -15,33 +15,33 @@ function test_beast_aca(h)
 
     X = lagrangecxd0(Γ)
 
+    println("Number of functions: ", numfunctions(X))
     # Compute system matrix here
 
     𝒱 = Helmholtz3D.singlelayer(wavenumber=0.0)
 
-    function singlelayerassembler(sdata, tdata)
+
+    @views function singlelayerassembler(Z, tdata, sdata)
         #Xt = subset(X,tdata)
         #Xs = subset(X,sdata)
 
         #return assemble(𝒱,Xt,Xs)
 
-        Z = zeros(Float64, length(tdata), length(sdata))
         store(v,m,n) = (Z[m,n] += v)
 
         blkasm = BEAST.blockassembler(𝒱,X,X)
         blkasm(tdata,sdata,store)
-        return Z
     end
 
-    ##
+    #
 
-    stree = create_tree(X.pos, nmin=40)
+    stree = create_tree(X.pos, nmin=400)
     @time hmat = HMatrix(singlelayerassembler, stree, stree, compressor=:aca)
 
-    ##
-    if numfunctions(X) <= 2000
-        𝗩 = assemble(𝒱,X,X)
-    end
+    #
+    #if numfunctions(X) <= 2000
+    #    𝗩 = assemble(𝒱,X,X)
+    #end
 
     @printf("Compression rate: %.2f %%\n", compressionrate(hmat)*100)
     return hmat
