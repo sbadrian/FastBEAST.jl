42161a10739d6bd86dd838de70463de35a2f51dc
Simon B. Adrian <simon.adrian@ieee.org>
2021-09-09 17:08:34 +0200

    Added IterativeSolver example

--- test/test_beast.jl
+++ test/test_beast.jl
@@ -2,6 +2,7 @@ using CompScienceMeshes
 using BEAST
 using LinearAlgebra
 using FastBEAST
+using IterativeSolvers
 
 CM = CompScienceMeshes
 
@@ -22,3 +23,67 @@ mat, hmat = test_beast_laplace_singlelayer(0.1)
 
 @test compressionrate(hmat) > 0.3
 @test estimate_reldifference(hmat,mat) ≈ 0 atol=1e-4
+
+#function test_beast_laplace_singlelayer_manufactured(h)
+
+# ##
+
+# mutable struct Monopole{T,P} <: BEAST.Functional
+#     location::P
+#     wavenumber::T
+#     amplitude::T
+# end
+  
+# function Monopole(p,k,a = 1)
+#     T = promote_type(eltype(p), typeof(k), typeof(a))
+#     P = similar_type(typeof(p), T)
+#     Monopole{T,P}(p,k,a)
+# end
+
+# monopolehh3d(;
+#     location    = error("missing arguement `location`"),
+#     wavenumber   = error("missing arguement `wavenumber`"),
+#     ) = Monopole(location, wavenumber)
+
+
+# function (f::Monopole)(r)
+#     p = f.location
+#     k = f.wavenumber
+#     a = f.amplitude
+#     return a * 1/norm(r-p)
+# end
+
+#     h=0.2
+#     Γ = CM.meshsphere(1, h)
+
+#     X = lagrangecxd0(Γ)
+    
+#     κ = 0.0
+#     𝒱 = Helmholtz3D.singlelayer(wavenumber=κ)
+#     q = Monopole(SVector(0.35,0.35,0.25), 0.0) #monopolehh3d(location = SVector(0.35,0.35,0.25), wavenumber = 0.0) 
+#     f = assemble(strace(q,Γ), X) 
+#     hmat = hassemble(𝒱,X,X, nmin=50)
+#     mat = assemble(𝒱,X,X)
+
+# ##
+#     hx, hch = cg(hmat,f, log=true,reltol=1e-4)
+
+#     x, ch = cg(mat,f, log=true,reltol=1e-4)
+# @test norm(hx - x)/norm(x) ≈ 0 atol=1e-3
+# ##
+#     Φ, Θ = [0.0], range(0,stop=π,length=100)
+#     pts = [point(cos(ϕ)*sin(θ), sin(ϕ)*sin(θ), cos(θ)) for ϕ in Φ for θ in Θ]
+#     ffd = potential(𝒱, pts, x, X)
+    
+# #end
+
+# ##
+# h=0.02
+# Γ = CM.meshsphere(1, h)
+
+# X = lagrangecxd0(Γ)
+
+# κ = 0.0
+# 𝒱 = Helmholtz3D.singlelayer(wavenumber=κ)
+# hmat = hassemble(𝒱,X,X, nmin=100)
+# println(compressionrate(hmat))
