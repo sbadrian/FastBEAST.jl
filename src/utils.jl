8d393357f06d984c180e389cafaf1052b131bc89
Simon B. Adrian <simon.adrian@ieee.org>
2024-05-14 14:51:28 +0200

    Add vector operation for norm and rel-diff estimator
    Badly needed in the case we deal with block operators, where some
    unfortunate implicit conversions are happening that are very slow.

--- src/utils.jl
+++ src/utils.jl
@@ -6,10 +6,12 @@ function estimate_norm(mat; tol=1e-4, itmax = 1000)
     i = 1
     σold = 1
     σnew = 1
+    @info "Estimate norm"
     while (norm(sqrt(σold)-sqrt(σnew))/norm(sqrt(σold)) > tol || i < itermin) && i < itmax
+        @info i, norm(sqrt(σold)-sqrt(σnew))/norm(sqrt(σold))
         σold = σnew
-        w = mat*v
-        x = adjoint(mat)*w
+        w = Vector(mat*v)
+        x = Vector(adjoint(mat)*w)
         σnew = norm(x)
         v = x/norm(x)
         i += 1
@@ -29,15 +31,17 @@ function estimate_reldifference(hmat::H, refmat; tol=1e-4) where {F, H <: Linear
     i = 1
     σold = 1
     σnew = 1
+    @info "Estimate norm of reference matrix"
     while norm(sqrt(σold)-sqrt(σnew))/norm(sqrt(σold)) > tol || i < itermin
+        @info i, norm(sqrt(σold)-sqrt(σnew))/norm(sqrt(σold))
         σold = σnew
-        w = hmat*v - refmat*v
-        x = adjoint(hmat)*w - adjoint(refmat)*w
-        σnew = norm(Vector(x))
+        w = Vector(hmat*v) - Vector(refmat*v)
+        x = Vector(adjoint(hmat)*w) - Vector(adjoint(refmat)*w)
+        σnew = norm(x)
         v = x/σnew
         i += 1
     end
-
+    @info "Estimate norm of reference matrix"
     norm_refmat = estimate_norm(refmat, tol=tol)
 
     return sqrt(σnew)/norm_refmat
